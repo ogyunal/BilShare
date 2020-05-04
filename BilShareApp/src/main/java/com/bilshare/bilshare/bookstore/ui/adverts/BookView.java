a1d22a04250b085dc44bcc3cd3d24e1ade188c8e
melih obut <melih.obut@ug.bilkent.edu.tr>
2020-05-05 02:51:35 +0300

    BookView tamamlandı, InventoryView'a bağlanmadı.

--- BilShareApp/src/main/java/com/bilshare/bilshare/bookstore/ui/adverts/BookView.java
+++ BilShareApp/src/main/java/com/bilshare/bilshare/bookstore/ui/adverts/BookView.java
@@ -1,15 +1,24 @@
 package com.bilshare.bilshare.bookstore.ui.adverts;
 
 import com.bilshare.bilshare.bookstore.ui.MainLayout;
+import com.bilshare.bilshare.bookstore.ui.signup.data.AvatarImage;
+import com.vaadin.flow.component.ComponentEvent;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.BeanValidationBinder;
+import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteConfiguration;
 import com.bilshare.bilshare.bookstore.backend.data.Book;
+import com.vaadin.flow.shared.Registration;
+
+import java.util.List;
 
 
 /**
@@ -24,37 +33,58 @@ public class BookView extends VerticalLayout {
 
     private Book book;
     private RouteConfiguration configuration = RouteConfiguration.forSessionScope();
+    H3 title = new H3("Book Extended Form");
+    Label name = new Label("Book Name");
+    Label author = new Label("Writer Name");
+    Label version = new Label("Version of the book");
+    Label relatedCourse = new Label("Category of the book is");
+    Label price = new Label("Price of the book is");
+    Label additionalInfo = new Label("Additional information");
+    AvatarImage photo = new AvatarImage();
+    Binder<Book> bookBinder = new BeanValidationBinder<>(Book.class);
 
     public BookView(){
         buildUI();
     }
 
     public void buildUI(){
-        setSizeFull();
+        bookBinder.bindInstanceFields(this);
 
-        Grid<Book> grid = new Grid<>(Book.class);
-        grid.setMaxWidth("800px");
-        grid.setWidth("100%");
-        setHorizontalComponentAlignment(Alignment.CENTER, grid);
-
-
-        grid.addItemClickListener(saveEvent -> {
-            Book toShow = saveEvent.getItem();
-
-        });
-
-        H3 title = new H3("Book Extended Form");
-        TextField bookNameField = new TextField("Book Name");
-        TextField writerNameField = new TextField("Writer Name");
-        TextField categoryField = new TextField("Category of the book is");
-        TextField priceField = new TextField("Price of the book is");
-        FormLayout form = new FormLayout(title, bookNameField, writerNameField, categoryField, priceField);
-
+        FormLayout form = new FormLayout(title, name, author, version, relatedCourse,price,additionalInfo);
 
         form.setMaxWidth("400px");
         form.getStyle().set("margin", "0 auto");
         add(form);
+
+        addClickShortcut(Key.ESCAPE);
+
+    }
+
+    public void setBook(Book book){
+        bookBinder.setBean(book );
+
+    }
+
+
+    // Events
+    public static abstract class BookViewEvent extends ComponentEvent<BookView> {
+        private Book book;
+
+        protected BookViewEvent(BookView bookView, Book book) {
+            super(bookView, false);
+            this.book = book;
+        }
+
+        public Book getBook() {
+            return book;
+        }
+    }
+
+    public <T extends ComponentEvent<?>> Registration addListener(Class<T> eventType,
+                                                                  ComponentEventListener<T> listener) {
+        return getEventBus().addListener(eventType, listener);
     }
 }
+
 
 
