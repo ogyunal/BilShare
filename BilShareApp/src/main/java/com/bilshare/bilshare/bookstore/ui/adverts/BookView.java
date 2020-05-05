657225d11a15622c82bec7ce152af3db436b9392
melih obut <melih.obut@ug.bilkent.edu.tr>
2020-05-06 02:26:27 +0300

    BBB

--- BilShareApp/src/main/java/com/bilshare/bilshare/bookstore/ui/adverts/BookView.java
+++ BilShareApp/src/main/java/com/bilshare/bilshare/bookstore/ui/adverts/BookView.java
@@ -2,6 +2,8 @@ package com.bilshare.bilshare.bookstore.ui.adverts;
 
 import com.bilshare.bilshare.bookstore.ui.MainLayout;
 import com.bilshare.bilshare.bookstore.ui.signup.data.AvatarImage;
+import com.bilshare.bilshare.bookstore.ui.signup.data.UserDetails;
+import com.bilshare.bilshare.bookstore.ui.signup.data.UserDetailsService;
 import com.vaadin.flow.component.ComponentEvent;
 import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.Key;
@@ -9,6 +11,7 @@ import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.BeanValidationBinder;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.PageTitle;
@@ -16,6 +19,7 @@ import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteConfiguration;
 import com.bilshare.bilshare.bookstore.backend.data.Book;
 import com.vaadin.flow.shared.Registration;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
@@ -25,28 +29,27 @@ import java.util.List;
  *
  */
 
-@Route(value = "Book", layout = MainLayout.class)
-@PageTitle("Book")
+
 public class BookView extends VerticalLayout {
 
     private Book book;
-    private RouteConfiguration configuration = RouteConfiguration.forSessionScope();
-    H3 title = new H3("Book Extended Form");
-    Label name = new Label("Book Name");
-    Label author = new Label("Writer Name");
-    Label version = new Label("Version of the book");
-    Label relatedCourse = new Label("Category of the book is");
-    Label price = new Label("Price of the book is");
-    Label additionalInfo = new Label("Additional information");
-    AvatarImage photo = new AvatarImage();
-    Binder<Book> bookBinder = new BeanValidationBinder<>(Book.class);
 
-    public BookView(){
+    H3 title = new H3("Book Extended Form");
+    TextField name = new TextField("Book Name");
+    TextField author = new TextField("Writer Name");
+    TextField version = new TextField("Version of the book");
+    TextField relatedCourse = new TextField("Category of the book is");
+    TextField price = new TextField("Price of the book is");
+    TextField additionalInfo = new TextField("Additional information");
+    AvatarImage photo = new AvatarImage();
+    Binder<Book> bookBinder;
+
+    public BookView(@Autowired Book book){
+        this.book=book;
         buildUI();
     }
 
     public void buildUI(){
-        bookBinder.bindInstanceFields(this);
 
         FormLayout form = new FormLayout(title, name, author, version, relatedCourse,price,additionalInfo);
 
@@ -56,11 +59,15 @@ public class BookView extends VerticalLayout {
 
         addClickShortcut(Key.ESCAPE);
 
-    }
+        bookBinder = new BeanValidationBinder<Book>(Book.class);
 
-    public void setBook(Book book){
-        bookBinder.setBean(book );
-
+        // Basic name fields that are required to fill in
+        bookBinder.forField(name).asRequired().bind("name");
+        bookBinder.forField(author).asRequired().bind("author");
+        bookBinder.forField(version).asRequired().bind("version");
+        bookBinder.forField(relatedCourse).asRequired().bind("course");
+        bookBinder.forField(price).bind("price");
+        bookBinder.forField(additionalInfo).bind("Info");
     }
 
 
