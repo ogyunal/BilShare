b3a81e045a96d0e700dabf6bb653c9c3ca3c1809
berkturkcapar <63585832+berkturkcapar@users.noreply.github.com>
2020-05-12 21:13:18 +0300

    Berk Update ProductRepo

---
+++ BilShareFinal/src/main/java/com/bilshare/ui/views/AddAdvert/AddAdvertView.java
@@ -0,0 +1,32 @@
+package com.bilshare.ui.views.AddAdvert;
+
+import com.bilshare.backend.service.ProductService;
+import com.bilshare.ui.MainLayout;
+import com.bilshare.ui.views.list.ProductForm;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Route(value = "addAdvert", layout = MainLayout.class)
+public class AddAdvertView extends VerticalLayout {
+
+    @Autowired
+    ProductService productService;
+
+    private ProductForm form;
+    private H3 header;
+
+
+    public AddAdvertView (ProductService productService){
+        this.productService = productService;
+        form = new ProductForm(productService);
+        form.createButtonsLayout();
+
+        header = new H3("New Advert Form");
+
+        add(header, form);
+        setAlignItems(Alignment.CENTER);
+        setJustifyContentMode(JustifyContentMode.CENTER);
+    }
+}
