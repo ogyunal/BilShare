201a86cf824d252c4ea1d7ada4dce98ed54d6d48
melih obut <melih.obut@ug.bilkent.edu.tr>
2020-05-19 19:41:17 +0300

    ListView

--- BilShareFinal/src/main/java/com/bilshare/ui/views/ContactUs/ContactUsView.java
+++ BilShareFinal/src/main/java/com/bilshare/ui/views/ContactUs/ContactUsView.java
@@ -1,52 +1,37 @@
 
 package com.bilshare.ui.views.ContactUs;
 
-
-import com.bilshare.backend.data.Category;
-import com.bilshare.backend.data.Type;
-import com.bilshare.backend.entity.Product;
-import com.bilshare.backend.service.ProductService;
 import com.bilshare.ui.MainLayout;
-import com.bilshare.ui.views.list.ProductForm;
-import com.vaadin.flow.component.*;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.combobox.ComboBox;
-import com.vaadin.flow.component.formlayout.FormLayout;
-import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.html.IFrame;
-import com.vaadin.flow.component.html.Span;
-import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.orderedlayout.FlexComponent;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.EmailField;
-import com.vaadin.flow.component.textfield.NumberField;
-import com.vaadin.flow.component.textfield.TextArea;
-import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.data.binder.BeanValidationBinder;
-import com.vaadin.flow.data.binder.Binder;
-import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import org.springframework.beans.factory.annotation.Autowired;
 
+/**
+ * Contact Us View  class which enables users to contact with the BilShare
+ * @author BilShare
+ * @version 1.0
+ */
 
 @Route(value = "contactUsView", layout = MainLayout.class)
 @PageTitle("Contact Us | BilShare")
 public class ContactUsView extends VerticalLayout {
 
 
+    //constructor
     public ContactUsView()
     {
         initView();
+        //creating an iframe
         IFrame iFrame = new IFrame();
         iFrame.setSrc("https://form.jotform.com/201366581719965");
         iFrame.setWidth("100%");
         iFrame.setHeight("1100px");
         add(iFrame);
     }
-
+    /**
+     * initView() setting alignment
+     */
     private void initView() {
         setDefaultHorizontalComponentAlignment(Alignment.STRETCH);
     }
