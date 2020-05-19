a66c2feb242955b6c6bf2dc146c676c61ad9c19b
berkturkcapar <63585832+berkturkcapar@users.noreply.github.com>
2020-05-19 18:40:38 +0300

    Berk Comment Ekleme

--- BilShareFinal/src/main/java/com/bilshare/ui/views/AddAdvert/AddAdvertView.java
+++ BilShareFinal/src/main/java/com/bilshare/ui/views/AddAdvert/AddAdvertView.java
@@ -12,33 +12,37 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
 
+/**
+ * AddAdvert view class which enables users to add new adverts to the inventory
+ * @author BilShare
+ * @version 1.0
+ */
+
 @Route(value = "addAdvert", layout = MainLayout.class)
 @PageTitle("Add Advert | BilShare")
 public class AddAdvertView extends VerticalLayout {
 
+    // properties
     @Autowired
     ProductService productService;
-
     private ProductForm form;
     private H3 header;
 
-
+    // constructor
     public AddAdvertView (ProductService productService){
+        // initializing
         this.productService = productService;
         form = new ProductForm(productService);
-        form.createButtonsLayout();
-
-
         header = new H3("New Advert Form");
 
+        // creating buttons
+        form.createButtonsLayout();
+
+        // adding the components
         add(header, form);
+
+        // setting alignment to center
         setAlignItems(Alignment.CENTER);
         setJustifyContentMode(JustifyContentMode.CENTER);
-    }
-
-    private void showSuccess() {
-        Notification notification = Notification.show("Your Advert Has Been Created ");
-        notification.addThemeVariants(NotificationVariant.LUMO_SUCCESS);
-        UI.getCurrent().navigate("");
     }
 }
