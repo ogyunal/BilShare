c27d75a1aad7a268ee2a025baf6aba3b5c45eeba
Oğulcan Ünal <ogulcan@ogyunal.com>
2020-05-19 21:24:30 +0300

    Update

--- bilshare-project/src/main/java/com/bilshare/bilshare/backend/repositories/CustomerRepository 2.java
+++
@@ -1,8 +0,0 @@
-package com.bilshare.bilshare.backend.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bilshare.bilshare.backend.data.entity.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
